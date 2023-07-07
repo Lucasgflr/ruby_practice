eee2380c2e61e97d7e6bfea4baef9d32d9657f50
Lucasgflr <137245984+Lucasgflr@users.noreply.github.com>
2023-07-06 21:19:11 -0300

    Create practice.rb

---
+++ practice.rb
@@ -0,0 +1,29 @@
+require 'mysql2'
+
+client = Mysql2::Client.new(
+  host: 'localhost',
+  username: 'root',
+  password: '',
+  database: 'database1'
+)
+
+#1
+results = client.query("SELECT * FROM people_<your_name>")
+
+#2
+p "First 10 records:"
+results.each_with_index do |row, index|
+  break if index >= 10
+  
+  puts "ID: #{row['id']}, Firstname: #{row['firstname']}, Lastname: #{row['lastname']}, Email: #{row['email']}"
+end
+
+#3
+doctor_count = client.query("SELECT COUNT(*) AS count FROM people_<your_name> WHERE profession = 'doctor'").first['count']
+p "Number of doctors: #{doctor_count}"
+
+#4
+client.query("UPDATE people_<your_name> SET email2 = REPLACE(email2, '@gmail.com', '@hotmail.com') WHERE profession = 'Ecologist'")
+p "Emails updated successfully!"
+
+client.close
