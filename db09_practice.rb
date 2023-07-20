86a7850f154b09ac4508961d0f72a6570792c590
Lucasgflr <137245984+Lucasgflr@users.noreply.github.com>
2023-07-20 17:13:00 -0300

    Update db09_practice.rb

--- db09_practice.rb
+++ db09_practice.rb
@@ -1,7 +1,9 @@
+require_relative 'methods.rb'
 require 'mysql2'
 require 'dotenv/load'
 
-client = Mysql2::Client.new(host: "db09.blockshopper.com", username: ENV['DB09_LGN'], password: ENV['DB09_PWD'], database: "applicant_tests")
 
+client = Mysql2::Client.new(host: "db09.blockshopper.com", username: "loki", password: "v4WmZip2K67J6Iq7NXC", database: "applicant_tests")
 
+get_teacher(1, client)
 client.close
